0e40779f35b39c0ce3834a02ceba377745fa90a1
fdev <facundo.migliorini@gmail.com>
2020-04-11 00:03:11 -0300

    Auth local strategy fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, ValidationPipe, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import LocalStrategyDto from './dto/LocalStrategyDto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
     
+    @UseGuards(AuthGuard('local'))
     @Post('/login')
-    @UsePipes(ValidationPipe)
-    async login(@Body() localStrategyDto: LocalStrategyDto): Promise<any> {
+    // @UsePipes(ValidationPipe)
+    // async login(@Body() localStrategyDto: LocalStrategyDto): Promise<any> {
         // return this.authService.validateUser();
+    // }
+    async login(@Request() req) {
+        return req.user;
     }
-    
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -20,7 +20,7 @@ export class User extends BaseEntity {
     @Column()
     username: string;
     
-    @Column({select: false})
+    @Column()
     password: string;
 
     @Column()
